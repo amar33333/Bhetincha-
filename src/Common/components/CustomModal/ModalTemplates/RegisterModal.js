819a4813b8f61d34807d74c27d4a9a7004d82592
wsamip <070bct536@ioe.edu.np>
2018-04-09 22:39:08 +0545

    changes in register modal

--- src/Common/components/CustomModal/ModalTemplates/RegisterModal.js
+++ src/Common/components/CustomModal/ModalTemplates/RegisterModal.js
@@ -11,19 +11,10 @@ import {
   FormGroup,
   Label
 } from "reactstrap";
-<<<<<<< HEAD
-=======
-import {
-  Icon,
-  Divider,
-  Button as ButtonS,
-  Checkbox,
-  Form
-} from "semantic-ui-react";
+
 import { connect } from "react-redux";
 
 import { onRegisterSubmit } from "../../../../actions";
->>>>>>> 0234b817569a1c6cc3dec083f41f0ac6a95022fd
 
 class RegisterModal extends Component {
   state = { username: "", password: "", email: "", business_name: "" };
@@ -52,71 +43,6 @@ class RegisterModal extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div>
-        <Form onSubmit={this.onFormSubmit}>
-          <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>
-                <i className="icon-user" />
-              </InputGroupText>
-            </InputGroupAddon>
-            <Input
-              type="text"
-              placeholder="Business Name"
-              value={this.state.business_name}
-              onChange={this.onChange.bind(this, "business_name")}
-            />
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>
-                <i className="icon-user" />
-              </InputGroupText>
-            </InputGroupAddon>
-            <Input
-              type="text"
-              placeholder="Username"
-              value={this.state.username}
-              onChange={this.onChange.bind(this, "username")}
-            />
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>@</InputGroupText>
-            </InputGroupAddon>
-            <Input
-              type="text"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={this.onChange.bind(this, "email")}
-            />
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>
-                <i className="icon-lock" />
-              </InputGroupText>
-            </InputGroupAddon>
-            <Input
-              type="password"
-              value={this.state.password}
-              onChange={this.onChange.bind(this, "password")}
-              placeholder="Password"
-            />
-          </InputGroup>
-          <InputGroup className="mb-4">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>
-                <i className="icon-lock" />
-              </InputGroupText>
-            </InputGroupAddon>
-            <Input type="password" placeholder="Repeat password" />
-          </InputGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox" /> I agree to{" "}
-=======
       <Form onSubmit={this.onFormSubmit}>
         <InputGroup className="mb-3">
           <InputGroupAddon addonType="prepend">
@@ -187,51 +113,54 @@ class RegisterModal extends Component {
             onChange={this.onChange.bind(this, "confirm_password")}
           />
         </InputGroup>
-        <Form.Field
-          control={Checkbox}
-          label={
-            <label>
-              I agree to{" "}
->>>>>>> 0234b817569a1c6cc3dec083f41f0ac6a95022fd
-              <a
-                href="http://techkunja.com.np"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                User Agreement
-              </a>{" "}
-              and{" "}
-              <a
-                href="http://techkunja.com.np"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Privacy Policy
-              </a>{" "}
-              of Bhetincha.
-            </Label>
-          </FormGroup>
+        <FormGroup check>
+          <Label for="checkbox2" sm={2}>
+            Checkbox
+          </Label>
+          <Col sm={{ size: 10 }}>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" id="checkbox2" /> I agree to{" "}
+                <a
+                  href="http://techkunja.com.np"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  User Agreement
+                </a>{" "}
+                and{" "}
+                <a
+                  href="http://techkunja.com.np"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Privacy Policy
+                </a>{" "}
+                of Bhetincha.
+              </Label>
+            </FormGroup>
+          </Col>
+        </FormGroup>
 
-          <Button
-            color="success"
-            block
-            //onClick={() => this.onRegisterBtnClick()}
-          >
-            Create Account
-          </Button>
+        <Button
+          color="success"
+          block
+          //onClick={() => this.onRegisterBtnClick()}
+        >
+          Create Account
+        </Button>
 
-          {/* <Divider horizontal>Or</Divider> */}
-          <Row className="modal_register__social__icons">
-            <button className="btn btn-facebook btn-md">
-              <span>facebook</span>
-            </button>
+        {/* <Divider horizontal>Or</Divider> */}
+        <Row className="modal_register__social__icons">
+          <button className="btn btn-facebook btn-md">
+            <span>facebook</span>
+          </button>
 
-            <button className="btn btn-google-plus btn-md">
-              <span>Google</span>
-            </button>
-          </Row>
-        </Form>
-      </div>
+          <button className="btn btn-google-plus btn-md">
+            <span>Google</span>
+          </button>
+        </Row>
+      </Form>
     );
   }
 }
