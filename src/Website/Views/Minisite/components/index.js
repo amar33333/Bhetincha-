d0e0949bf4f0da995450439671447a2741dec0f4
wsamip <070bct536@ioe.edu.np>
2018-04-18 13:59:17 +0545

    gallery page and route added

--- src/Website/Views/Minisite/components/index.js
+++ src/Website/Views/Minisite/components/index.js
@@ -1,14 +1,6 @@
 import BusinessFooter from "./BusinessFooter";
 import BusinessNav from "./BusinessNav";
-import AboutEditor from "./AboutEditor";
 import Gallery from "./Gallery";
 import AboutUs from "./AboutUs";
 import CoverPhoto from "./CoverPhoto";
-export {
-  BusinessFooter,
-  BusinessNav,
-  AboutUs,
-  CoverPhoto,
-  AboutEditor,
-  Gallery
-};
+export { BusinessFooter, BusinessNav, AboutUs, CoverPhoto, Gallery };
