bdd0f3c51d5ce2df5fb5e62a06e12692b9698c4a
samip874 <samip.mteverest@gmail.com>
2018-05-03 12:41:08 +0545

    google map dashboard and key in iterable things

--- src/Admin/views/Dashboard/Dashboard.js
+++ src/Admin/views/Dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import {
   Marker
 } from "react-google-maps";
 
+import { Container, Row, Col, Input, Button } from "reactstrap";
+
 const MyMapComponent = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap
@@ -27,42 +29,53 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        Hello Dashboard
-        <input
-          value={this.state.address}
-          onChange={event => this.setState({ address: event.target.value })}
-        />
-        <button
-          onClick={() => {
-            let geocoder = new window.google.maps.Geocoder();
-            geocoder.geocode(
-              { address: this.state.address },
-              (results, status) => {
-                //console.log(results, status[0]);
-                const location = results[0].geometry.location;
-                status === "OK" &&
-                  this.setState({
-                    position: { lat: location.lat(), lng: location.lng() }
-                  });
-              }
-            );
-          }}
-        >
-          Find in map!
-        </button>
-        <MyMapComponent
-          position={this.state.position}
-          onDragEnd={({ latLng }) => {
-            this.setState({
-              position: { lat: latLng.lat(), lng: latLng.lng() }
-            });
-            console.log(latLng.lat(), latLng.lng());
-          }}
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuyvcH5Rr55zHXmJFjuQ30jd_edLi1HbQ"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+        <Container>
+          <Row className="mb-3">
+            <Col xs="12" md="6">
+              <Input
+                value={this.state.address}
+                onChange={event =>
+                  this.setState({ address: event.target.value })
+                }
+              />
+            </Col>
+            <Col xs="12" md="2">
+              <Button
+                color="primary"
+                onClick={() => {
+                  let geocoder = new window.google.maps.Geocoder();
+                  geocoder.geocode(
+                    { address: this.state.address },
+                    (results, status) => {
+                      //console.log(results, status[0]);
+                      if (status === "OK") {
+                        const location = results[0].geometry.location;
+                        this.setState({
+                          position: { lat: location.lat(), lng: location.lng() }
+                        });
+                      }
+                    }
+                  );
+                }}
+              >
+                Find in map!
+              </Button>
+            </Col>
+          </Row>
+          <MyMapComponent
+            position={this.state.position}
+            onDragEnd={({ latLng }) => {
+              this.setState({
+                position: { lat: latLng.lat(), lng: latLng.lng() }
+              });
+              console.log(latLng.lat(), latLng.lng());
+            }}
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuyvcH5Rr55zHXmJFjuQ30jd_edLi1HbQ"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        </Container>
       </div>
     );
   }
