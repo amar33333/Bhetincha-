d9a06bf85af883096bf2cabbd1e9def5cee19956
sabbiu <070bct531@ioe.edu.np>
2018-05-04 14:26:16 +0545

    gallery minisite changed to epics

--- src/Website/Views/Minisite/actions/crudActions.js
+++ src/Website/Views/Minisite/actions/crudActions.js
@@ -46,19 +46,34 @@ epics.push((action$, { getState }) =>
     const globalState = getState();
     let TYPE;
     const extraDispatch = [];
-    if (updates[0] === "cover_photo" || updates[0] === "logo") {
-      TYPE = {
-        pending: UPDATE_LOGO_COVER_PHOTO_PENDING,
-        fulfilled: UPDATE_LOGO_COVER_PHOTO_FULFILLED,
-        rejected: UPDATE_LOGO_COVER_PHOTO_REJECTED
-      };
-    } else if (updates[0] === "about") {
-      TYPE = {
-        pending: UPDATE_ABOUT_PENDING,
-        fulfilled: UPDATE_ABOUT_FULFILLED,
-        rejected: UPDATE_ABOUT_REJECTED
-      };
-      extraDispatch.push({ type: TOGGLE_EDIT_ABOUT_US });
+    switch (updates[0]) {
+      case "cover_photo":
+      case "logo":
+        TYPE = {
+          pending: UPDATE_LOGO_COVER_PHOTO_PENDING,
+          fulfilled: UPDATE_LOGO_COVER_PHOTO_FULFILLED,
+          rejected: UPDATE_LOGO_COVER_PHOTO_REJECTED
+        };
+        break;
+      case "about":
+        TYPE = {
+          pending: UPDATE_ABOUT_PENDING,
+          fulfilled: UPDATE_ABOUT_FULFILLED,
+          rejected: UPDATE_ABOUT_REJECTED
+        };
+        extraDispatch.push({ type: TOGGLE_EDIT_ABOUT_US });
+        break;
+
+      case "albums":
+        TYPE = {
+          pending: CREATE_NEW_ALBUM_PENDING,
+          fulfilled: CREATE_NEW_ALBUM_FULFILLED,
+          rejected: CREATE_NEW_ALBUM_REJECTED
+        };
+        break;
+
+      default:
+        return Observable.of({ type: UPDATE_BUSINESS_REJECTED });
     }
 
     return onBusinessEachPutAjax({
@@ -86,72 +101,34 @@ epics.push((action$, { getState }) =>
   })
 );
 
-export const createNewAlbum = ({
-  data,
-  id,
-  username,
-  access_token
-}) => dispatch => {
-  dispatch({ type: CREATE_NEW_ALBUM_PENDING });
-  onBusinessEachPut({
-    id,
-    access_token,
-    data
-  })
-    .then(response => {
-      response.data.msg === "success" &&
-        onBusinessEachGet({ username })
-          .then(innerResponse => {
-            const data = innerResponse.data;
-            const payload = {};
-            payload.albums = data.albums;
+export const handleGalleryPhotoUpload = payload => ({
+  type: UPLOAD_GALLERY_PHOTO_PENDING,
+  payload
+});
 
-            dispatch({
-              type: CREATE_NEW_ALBUM_FULFILLED,
-              payload
-            });
-          })
-          .catch(error => dispatch({ type: CREATE_NEW_ALBUM_REJECTED }));
-    })
-    .catch(error => dispatch({ type: CREATE_NEW_ALBUM_REJECTED }));
-};
-
-export const handleGalleryPhotoUpload = ({
-  photos,
-  business_id,
-  album_id,
-  username,
-  access_token
-}) => dispatch => {
-  dispatch({ type: UPLOAD_GALLERY_PHOTO_PENDING, payload: album_id });
-  onBusinessEachAlbumEachPhotos({
-    business_id,
-    album_id,
-    access_token,
-    data: {
-      photos: photos.map(photo => ({
-        name: photo.name,
-        data: photo.base64
-      }))
-    }
-  })
-    .then(response => {
-      response.data.msg === "success" &&
-        onBusinessEachGet({ username })
-          .then(innerResponse => {
-            const data = innerResponse.data;
-            const payload = {};
-            payload.albums = data.albums;
-
-            dispatch({
-              type: UPLOAD_GALLERY_PHOTO_FULFILLED,
-              payload
-            });
-          })
-          .catch(error => dispatch({ type: UPLOAD_GALLERY_PHOTO_REJECTED }));
-    })
-    .catch(error => dispatch({ type: UPLOAD_GALLERY_PHOTO_REJECTED }));
-};
+epics.push((action$, { getState }) =>
+  action$.ofType(UPLOAD_GALLERY_PHOTO_PENDING).mergeMap(({ payload }) =>
+    onBusinessEachAlbumEachPhotosAjax({
+      body: payload.body,
+      album_id: payload.album_id,
+      access_token: getState().auth.cookies.token_data.access_token,
+      business_id: getState().MinisiteContainer.crud.id
+    }).map(
+      ({ response }) =>
+        response.msg === "success"
+          ? {
+              type: FETCH_PART_BUSINESS,
+              updates: ["albums"],
+              onSuccess: payload => [
+                { type: UPLOAD_GALLERY_PHOTO_FULFILLED, payload }
+              ],
+              FAILED: UPLOAD_GALLERY_PHOTO_REJECTED,
+              slug: getState().auth.cookies.user_data.slug
+            }
+          : { type: UPLOAD_GALLERY_PHOTO_REJECTED }
+    )
+  )
+);
 
 epics.push(action$ =>
   action$.ofType(FETCH_PART_BUSINESS).mergeMap(action => {
