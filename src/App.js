f75be5df0ecd2aebc08e1f7ffc53132b4be2f862
sabbiu <070bct531@ioe.edu.np>
2018-05-19 20:41:49 +0545

    sass modified

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 // Import Flag Icons Set
-import "flag-icon-css/css/flag-icon.min.css";
+// import "flag-icon-css/css/flag-icon.min.css";
 // Import Font Awesome Icons Set
 import "font-awesome/css/font-awesome.min.css";
 // Import Simple Line Icons Set
 import "simple-line-icons/css/simple-line-icons.css";
 
 import "react-toastify/dist/ReactToastify.css";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./static/scss/style.css";
 // import "../node_modules/@coreui/styles/scss/_dropdown-menu-right.scss";
