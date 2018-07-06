61ff661c01a1910d8c5c092481443fd4df89a90e
sabbiu <070bct531@ioe.edu.np>
2018-07-06 15:24:47 +0545

    image editor done

--- src/Website/Views/Minisite/components/CoverPhoto/PhotoEditor.js
+++ src/Website/Views/Minisite/components/CoverPhoto/PhotoEditor.js
@@ -101,19 +101,20 @@ class PhotoEditor extends Component {
                 <FormGroup>
                   <Button
                     color="primary"
+                    data-tooltip="Add new photo to edit"
                     onClick={() => this[`${this.state.active}UploadEl`].click()}
                   >
                     Add New Photo
                   </Button>
                 </FormGroup>
                 <FormGroup>
-                  <Input
+                  <input
                     style={{ display: "none" }}
                     ref={ref => (this.logoUploadEl = ref)}
                     type="file"
                     onChange={this.handleUploadPhoto}
                   />
-                  <Input
+                  <input
                     style={{ display: "none" }}
                     ref={ref => (this.coverUploadEl = ref)}
                     type="file"
@@ -121,31 +122,38 @@ class PhotoEditor extends Component {
                   />
                 </FormGroup>
               </Row>
-              <AvatarEditor
-                ref={ref => (this[`imageEditorEl`] = ref)}
-                width={this.state[this.state.active].WIDTH}
-                height={this.state[this.state.active].HEIGHT}
-                scale={this.state[this.state.active].zoomSlider / 100}
-                image={
-                  this.state[this.state.active].files.length === 0
-                    ? this.props[this.state.active]
-                    : this.state[this.state.active].files[0]
-                }
-                //crossOrigin="anonymous"
-              />
-              <div style={{ margin: "20px", width: "160px" }}>
-                <span>Zoom</span>
-                <Slider
-                  min={80}
-                  max={300}
-                  value={this.state[this.state.active].zoomSlider}
-                  onChange={this.handleSliderChange}
+              <div>
+                <AvatarEditor
+                  ref={ref => (this[`imageEditorEl`] = ref)}
+                  width={this.state[this.state.active].WIDTH}
+                  height={this.state[this.state.active].HEIGHT}
+                  scale={this.state[this.state.active].zoomSlider / 100}
+                  image={
+                    this.state[this.state.active].files.length === 0
+                      ? this.props[this.state.active]
+                      : this.state[this.state.active].files[0]
+                  }
+                  //crossOrigin="anonymous"
                 />
+                {this.state[this.state.active].files.length !== 0 && (
+                  <div style={{ margin: "20px", width: "160px" }}>
+                    <span>Zoom</span>
+                    <Slider
+                      min={80}
+                      max={300}
+                      value={this.state[this.state.active].zoomSlider}
+                      onChange={this.handleSliderChange}
+                    />
+                  </div>
+                )}
               </div>
             </div>
             <Button
               color="success"
-              disabled={this.props.loading}
+              disabled={
+                this.props.loading ||
+                this.state[this.state.active].files.length === 0
+              }
               onClick={this.onImageSave}
             >
               Upload
