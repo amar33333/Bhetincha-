add65719f481c5fd3c09f44ac3d1535aca7633d9
sabbiu <070bct531@ioe.edu.np>
2018-08-02 16:49:17 +0545

    caching removed index.html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import "./config/rxjs";
 import "./index.css";
 import store from "./config/store";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 import * as firebase from "firebase";
 
@@ -35,4 +36,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
